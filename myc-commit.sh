23f3e394f642a1dc89f3d8a047e8e813cb11e5f1
Aniket-coder95 <2019pis5014@mnit.ac.in>
2022-06-06 13:57:18 +0530

    codeupdated

--- myc-commit.sh
+++ myc-commit.sh
@@ -1,6 +1,5 @@
 echo "What is the commit Message ?"
 read codeupdate
 sudo git add .
-# echo $codeupdate
-sudo git commit -m '$codeupdated'
+sudo git commit -m codeupdated
 sudo git push origin main
