fb241ab87af1291efdef1cc7cd6eb8d603fc47a8
Aniket-coder95 <2019pis5014@mnit.ac.in>
2022-06-06 13:09:30 +0530

    codeupdated

--- test.c
+++ test.c
@@ -1,32 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 int main()
-{
-    char str[30],*p1,*p2;
-    printf("Enter any string\n");
-    fgets(str, sizeof(str), stdin);
-    p1=str+strlen(str);
-    p2=str;
-    p1--;
-    while(p1>p2)
-    {
-        if(*p1==*p2)
-        {
-            p1--;
-            p2++;
-        }
-        else
-        {
-            break;
-        }
-    }
-        if(*p2 == *p1 || *p2 == *(p1-1))
-        {
-            printf("The string %s is a Palindrome",str);
-        }
-        else
-        {
-            printf("The string %s is not a Palindrome",str);
-        }
-    return 0;
+{     
+  printf("The string is not a Palindrome\n");
+        
+  return 0;
 }
