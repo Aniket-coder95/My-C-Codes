51ae17018561e03e6722bcfb383532197e902741
Aniket-coder95 <2019pis5014@mnit.ac.in>
2022-06-06 14:01:32 +0530

    $codeupdated

--- myc-commit.sh
+++ myc-commit.sh
@@ -1,5 +1,5 @@
 echo "What is the commit Message ?"
 read codeupdate
 sudo git add .
-sudo git commit -m codeupdated
+sudo git commit -m '$codeupdated'
 sudo git push origin main
--- test.c
+++ test.c
@@ -3,6 +3,5 @@
 int main()
 {     
   printf("The string is not a Palindrome\n");
-        
   return 0;
 }
